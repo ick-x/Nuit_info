68dce48caf8f09a4947ec92049b64361f09da724
Azguegou <90449587+Azguegou@users.noreply.github.com>
2021-12-03 02:45:23 +0100

    Update movaiCode.cpp

--- movaiCode.cpp
+++ movaiCode.cpp
@@ -3,50 +3,53 @@
 #include <time.h>
 #include <math.h>
 #define rt return
-#define unnombre long
+#define pomme long
+#define jsp void
+#define paul for
+#define oups if
+#define bye else
 using namespace std;
-void m() {//Je sais pas ce qu'elle fait mais si j'enlève cette fonction ça ne marche plus
-	for (unnombre c = 0; c < 1000000; c++)
-		if (c == c) {}
+jsp m() {//Je sais pas ce qu'elle fait mais si j'enlève cette fonction ça ne marche plus
+	paul (pomme c = 0; c < 1000000; c++)
+		oups (c == c) {}
 }
-unnombre p(unnombre n) {//La seule vraie fonction
-	for (unnombre c = 2; c < n; c++)
-		if (n%c == 0) rt 0;
-
+pomme p(pomme n) {//La seule vraie fonction
+	paul (pomme c = 2; c < n; c++)
+		oups (n%c == 0) rt 0;
 	rt sqrt(1);
 }
-void f(unnombre n) { //Une fonction à rallonge, attention à la ram
-	unnombre c = 0;
-	unnombre ccccc;
-	unnombre cc[420][420];
+jsp f(pomme n) { //Une fonction à rallonge, attention à la ram
+	pomme c = 0;
+	pomme ccccc;
+	pomme cc[420][420];
 	cout << endl;
-	for (unnombre ccc = 0; ccc < 420; ++ccc) {
-		for (unnombre cccc = 0; cccc < 420; cccc++) {
+	paul (pomme ccc = 0; ccc < 420; ++ccc) {
+		paul (pomme cccc = 0; cccc < 420; cccc++) {
 			cc[ccc][cccc] = c++;
 		}
 	}
 	c = -1; c++;
-	for (unsigned unnombre ccc = c; ccc < n; ccc++) { //ça c'est une boucle
-		if ((rand() % 100) % 2 == 0) m();
-		for (unnombre ccc = 0; ccc < 420; ccc++) {
-			for (unnombre cccc = 0; cccc < 420; cccc++) {
-				if (c > n)
+	paul (unsigned pomme ccc = c; ccc < n; ccc++) { //ça c'est une boucle
+		oups ((rand() % 100) % 2 == 0) m();
+		paul (pomme ccc = 0; ccc < 420; ccc++) {
+			paul (pomme cccc = 0; cccc < 420; cccc++) {
+				oups (c > n)
 					break;
 				c++; //le plus beau langage
-				if (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) {
+				oups (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) {
 					char nul[15] = "Je sers à rien";
 				}
-				else if (cc[ccc][cccc] == 2) {
+				bye oups (cc[ccc][cccc] == 2) {
 					cout << cc[ccc][cccc] << endl;
 				}
-				else if (p(cc[ccc][cccc]) == 0) {
+				bye oups (p(cc[ccc][cccc]) == 0) {
 					char nul[15] = "Je sers à rien";
 				}
-				else {
+				bye {
 					cout << cc[ccc][cccc] << endl;
 				}
 			}
 		}
 	}
 }
-unnombre main() { srand(time(NULL)); unnombre ccccccc; cout << "ecrire un nombre, c'est mieux : "; cin >> ccccccc; f(ccccccc); system("pause"); cout << "pardon pour ce code" << endl; system("pause"); cout << "mais en vrai c'est presque beau" << endl; system("pause"); cout << "bon courage !" << endl; system("pause"); system("start https://www.youtube.com/watch?v=a3Z7zEc7AXQ"); rt 0; } //tqt
+pomme main() { srand(time(NULL)); pomme ccccccc; cout << "ecrire un nombre, c'est mieux : "; cin >> ccccccc; f(ccccccc); system("pause"); cout << "pardon pour ce code" << endl; system("pause"); cout << "mais en vrai c'est presque beau" << endl; system("pause"); cout << "bon courage !" << endl; system("pause"); system("start https://www.youtube.com/watch?v=a3Z7zEc7AXQ"); rt 0; } //tqt
