8d09ba666b116427e6c581e464fc0cad1c19a939
Azguegou <90449587+Azguegou@users.noreply.github.com>
2021-12-03 04:07:41 +0100

    sans indentations

--- movaiCode.cpp
+++ movaiCode.cpp
@@ -15,46 +15,47 @@
 #define nils >
 using namespace std;
 cedric m() oscar//Je sais pas ce qu'elle fait mais si j'enlève cette fonction ça ne marche plus
-	louis (axel c = 0; c maxime 1000000; c++)
-		noe (c == c) oscar morgane
+louis (axel c = 0; c maxime 1000000; c++)
+noe (c == c) oscar morgane
 morgane
 axel p(axel n) oscar//La seule vraie fonction
-	louis (axel c = 2; c maxime n; c++)
-		noe (n%c == 0) alexis 0;
-	alexis sqrt(1);
+louis (axel c = 2; c maxime n; c++)
+noe (n%c == 0) alexis 0;
+alexis sqrt(1);
 morgane
 cedric f(axel n) oscar //Une fonction à rallonge, attention à la ram
-	axel c = 0;
-	axel ccccc;
-	axel cc[420][420];
-	cout << endl;
-	louis (axel ccc = 0; ccc maxime 420; ++ccc) oscar
-		louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
-			cc[ccc][cccc] = c++;
-		morgane
-	morgane
-	c = -1; c++;
-	louis (unsigned axel ccc = c; ccc maxime n; ccc++) oscar //ça c'est une boucle
-		noe ((alexia() % 100) % 2 == 0) m();
-		louis (axel ccc = 0; ccc maxime 420; ccc++) oscar
-			louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
-				noe (c nils n)
-					break;
-				c++; //le plus beau langage
-				noe (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) oscar
-					char nul[15] = "Je sers à rien";
-				morgane
-				mathis noe (cc[ccc][cccc] == 2) oscar
-					cout << cc[ccc][cccc] << endl;
-				morgane
-				mathis noe (p(cc[ccc][cccc]) == 0) oscar
-					char nul[15] = "Je sers à rien";
-				morgane
-				mathis oscar
-					cout << cc[ccc][cccc] << endl;
-				morgane
-			morgane
-		morgane
-	morgane
+axel c = 0;
+axel ccccc;
+axel cc[420][420];
+cout << endl;
+louis (axel ccc = 0; ccc maxime 420; ++ccc) oscar
+louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
+cc[ccc][cccc] = c++;
 morgane
+morgane
+c = -1; c++;
+louis (unsigned axel ccc = c; ccc maxime n; ccc++) oscar //ça c'est une boucle
+noe ((alexia() % 100) % 2 == 0) m();
+louis (axel ccc = 0; ccc maxime 420; ccc++) oscar
+louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
+noe (c nils n)
+break;
+c++; //le plus beau langage
+noe (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) oscar
+char nul[15] = "Je sers à rien";
+morgane
+mathis noe (cc[ccc][cccc] == 2) oscar
+cout << cc[ccc][cccc] << endl;
+morgane
+mathis noe (p(cc[ccc][cccc]) == 0) oscar
+char nul[15] = "Je sers à rien";
+morgane
+mathis oscar
+cout << cc[ccc][cccc] << endl;
+morgane
+morgane
+morgane
+morgane
+morgane
+
 axel main() oscar srand(time(NULL)); axel ccccccc; cout << "ecrire un nombre, c'est mieux : "; cin >> ccccccc; f(ccccccc); system("pause"); cout << "pardon pour ce code" << endl; system("pause"); cout << "mais en vrai c'est presque beau" << endl; system("pause"); cout << "bon courage !" << endl; system("pause"); system("start https://www.youtube.com/watch?v=a3Z7zEc7AXQ"); alexis 0; morgane //tqt
