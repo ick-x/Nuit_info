4b740af4b935b8030f5613ace31ad60899b14ad5
Azguegou <90449587+Azguegou@users.noreply.github.com>
2021-12-03 03:21:58 +0100

    Avec nos noms c'est mieux

--- movaiCode.cpp
+++ movaiCode.cpp
@@ -2,54 +2,59 @@
 #include <random>
 #include <time.h>
 #include <math.h>
-#define rt return
-#define pomme long
-#define jsp void
-#define paul for
-#define oups if
-#define bye else
+#define alexis return
+#define axel long
+#define cedric void
+#define louis for
+#define noe if
+#define mathis else
+#define alexia rand
+#define maxime <
+#define oscar {
+#define morgane }
+#define nils >
 using namespace std;
-jsp m() {//Je sais pas ce qu'elle fait mais si j'enlève cette fonction ça ne marche plus
-	paul (pomme c = 0; c < 1000000; c++)
-		oups (c == c) {}
-}
-pomme p(pomme n) {//La seule vraie fonction
-	paul (pomme c = 2; c < n; c++)
-		oups (n%c == 0) rt 0;
-	rt sqrt(1);
-}
-jsp f(pomme n) { //Une fonction à rallonge, attention à la ram
-	pomme c = 0;
-	pomme ccccc;
-	pomme cc[420][420];
+cedric m() oscar//Je sais pas ce qu'elle fait mais si j'enlève cette fonction ça ne marche plus
+	louis (axel c = 0; c maxime 1000000; c++)
+		noe (c == c) oscar morgane
+morgane
+axel p(axel n) oscar//La seule vraie fonction
+	louis (axel c = 2; c maxime n; c++)
+		noe (n%c == 0) alexis 0;
+	alexis sqrt(1);
+morgane
+cedric f(axel n) oscar //Une fonction à rallonge, attention à la ram
+	axel c = 0;
+	axel ccccc;
+	axel cc[420][420];
 	cout << endl;
-	paul (pomme ccc = 0; ccc < 420; ++ccc) {
-		paul (pomme cccc = 0; cccc < 420; cccc++) {
+	louis (axel ccc = 0; ccc maxime 420; ++ccc) oscar
+		louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
 			cc[ccc][cccc] = c++;
-		}
-	}
+		morgane
+	morgane
 	c = -1; c++;
-	paul (unsigned pomme ccc = c; ccc < n; ccc++) { //ça c'est une boucle
-		oups ((rand() % 100) % 2 == 0) m();
-		paul (pomme ccc = 0; ccc < 420; ccc++) {
-			paul (pomme cccc = 0; cccc < 420; cccc++) {
-				oups (c > n)
+	louis (unsigned axel ccc = c; ccc maxime n; ccc++) oscar //ça c'est une boucle
+		noe ((alexia() % 100) % 2 == 0) m();
+		louis (axel ccc = 0; ccc maxime 420; ccc++) oscar
+			louis (axel cccc = 0; cccc maxime 420; cccc++) oscar
+				noe (c nils n)
 					break;
 				c++; //le plus beau langage
-				oups (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) {
+				noe (cc[ccc][cccc] == 0 | cc[ccc][cccc] == 1) oscar
 					char nul[15] = "Je sers à rien";
-				}
-				bye oups (cc[ccc][cccc] == 2) {
+				morgane
+				mathis noe (cc[ccc][cccc] == 2) oscar
 					cout << cc[ccc][cccc] << endl;
-				}
-				bye oups (p(cc[ccc][cccc]) == 0) {
+				morgane
+				mathis noe (p(cc[ccc][cccc]) == 0) oscar
 					char nul[15] = "Je sers à rien";
-				}
-				bye {
+				morgane
+				mathis oscar
 					cout << cc[ccc][cccc] << endl;
-				}
-			}
-		}
-	}
-}
-pomme main() { srand(time(NULL)); pomme ccccccc; cout << "ecrire un nombre, c'est mieux : "; cin >> ccccccc; f(ccccccc); system("pause"); cout << "pardon pour ce code" << endl; system("pause"); cout << "mais en vrai c'est presque beau" << endl; system("pause"); cout << "bon courage !" << endl; system("pause"); system("start https://www.youtube.com/watch?v=a3Z7zEc7AXQ"); rt 0; } //tqt
+				morgane
+			morgane
+		morgane
+	morgane
+morgane
+axel main() oscar srand(time(NULL)); axel ccccccc; cout << "ecrire un nombre, c'est mieux : "; cin >> ccccccc; f(ccccccc); system("pause"); cout << "pardon pour ce code" << endl; system("pause"); cout << "mais en vrai c'est presque beau" << endl; system("pause"); cout << "bon courage !" << endl; system("pause"); system("start https://www.youtube.com/watch?v=a3Z7zEc7AXQ"); alexis 0; morgane //tqt
